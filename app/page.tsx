79dd60584ec1bf449f11ac92c6bbc799370386f0
Amir <altghaderi@gmail.com>
2023-12-29 12:58:18 -0700

    Used the api

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import useSWR from "swr";
 
 export default function Home() {
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+  const { data, error } = useSWR("/api/2/3", fetcher);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       This is the home page
+      {data && <div>{data}</div>}
     </main>
   );
 }
